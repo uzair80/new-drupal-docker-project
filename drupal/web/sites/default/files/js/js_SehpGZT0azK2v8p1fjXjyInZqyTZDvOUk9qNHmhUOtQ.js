4e676b6b22f023d2366f2096c42a6f30cdd7aca5
uzair <uzair21180@gmail.com>
2023-01-25 14:24:14 +0500

    drupal docker files

---
+++ drupal/web/sites/default/files/js/js_SehpGZT0azK2v8p1fjXjyInZqyTZDvOUk9qNHmhUOtQ.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.latest_site = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
+;
---
+++ drupal/web/themes/custom/latest_site/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.latest_site = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
